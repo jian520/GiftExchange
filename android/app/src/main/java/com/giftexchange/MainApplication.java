68781b80f1aa0247d7afe386ad8d1b1471a7bbe1
张建 <jian.zhang@forufamily.com>
2019-01-05 10:25:48 +0800

    no message

--- android/app/src/main/java/com/giftexchange/MainApplication.java
+++ android/app/src/main/java/com/giftexchange/MainApplication.java
@@ -3,7 +3,6 @@ package com.giftexchange;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.beefe.picker.PickerViewPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -26,7 +25,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new PickerViewPackage(),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage()
       );
