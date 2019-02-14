07b171d92ba92e96410b3863c12d1b85ce734de2
张建 <jian.zhang@forufamily.com>
2019-02-14 17:42:42 +0800

    no message

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+//react-native bundle --entry-file index.js --platform ios --dev false --bundle-output release_ios/main.jsbundle --assets-dest release_ios/
